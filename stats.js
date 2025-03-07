d46ef65db470e7cf7624857bc20df9bfeb4801f4
nams93 <108931831+nams93@users.noreply.github.com>
2025-03-07 11:09:37 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".btn").addEventListener("click", function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut
+        window.location.href = "index.html"; // Redirection forcée
+    });
+});
 
